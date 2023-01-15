8eb66ffeee10e4dafb44c133170c5e6375fdce10
hd4934 <hd4934@exo.att.com>
2023-01-16 00:42:04 +0700

    update models and migrations

--- yfj/app/models.py
+++ yfj/app/models.py
@@ -1,19 +1,19 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, Enum, DateTime, ForeignKey, Float, CheckConstraint
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship, backref
 from uuid import uuid4
 from app.database import db
+import enum
 
-class UserCategory(db.Model):
-    __tablename__ = "user_category"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    name = Column(String(64), unique = True, nullable=False)
+class UserCategoryEnum(enum.Enum):
+    Student="Student"
+    Volunteer="Volunteer"
 
 class User(db.Model):
     __tablename__ = "user"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     username = Column(String(128), unique = True, nullable=False)
-    category_id = Column(UUID(as_uuid=True), ForeignKey("user_category.id"))
+    role = Column(Enum(UserCategoryEnum, create_constraint=True, name="user_category_enum"), nullable=False)
     math = Column(Float, nullable=False)
     physics = Column(Float, nullable=False)
     chemistry = Column(Float, nullable=False)
@@ -31,7 +31,15 @@ class Job(db.Model):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     name = Column(String(128), unique=True, nullable=False)
     average_earning = Column(Integer, nullable=False)
-    user = relationship("User", secondary="user_job")
+    users = relationship("User", secondary="user_job")
+
+    def __init__(self, name, average_earning):
+        self.name = name
+        self.average_earning = average_earning
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
 
 class UserJob(db.Model):
     __tablename__ = "user_job"
